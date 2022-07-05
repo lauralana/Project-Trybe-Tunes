5c0500a13ef2da4cf942fcb19a787b4a2d57a8ef
Laura Lana <lauramarqueslana@gmail.com>
2022-07-04 23:41:37 -0300

    Add req 07

---
+++ src/components/MusicCard.jsx
@@ -0,0 +1,24 @@
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+
+export default class MusicCard extends Component {
+  render() {
+    const { trackName, previewUrl } = this.props;
+    return (
+      <div>
+        <h3>{trackName}</h3>
+        <audio data-testid="audio-component" src={ previewUrl } controls>
+          <track kind="captions" />
+          O seu navegador não suporta o elemento
+          <code>audio</code>
+          .
+        </audio>
+      </div>
+    );
+  }
+}
+
+MusicCard.propTypes = {
+  trackName: PropTypes.string,
+  previewUrl: PropTypes.string,
+}.isRequired;
--- src/pages/Album.jsx
+++ src/pages/Album.jsx
@@ -1,12 +1,55 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
+import MusicCard from '../components/MusicCard';
+import getMusics from '../services/musicsAPI';
 
 export default class Album extends Component {
-  render() {
-    return (
-      <div data-testid="page-album">
-        <Header />
-      </div>
-    );
+  constructor() {
+    super();
+    this.state = {
+      collection: '',
+      songs: [],
+      artist: '',
+    };
   }
+
+  componentDidMount() {
+    this.funcFetchGetMusics();
+  }
+
+funcFetchGetMusics = async () => {
+  const { match: { params: { id } } } = this.props;
+  const music = await getMusics(id);
+  this.setState({
+    collection: music[0].collectionName,
+    artist: music[0].artistName,
+    songs: music,
+  });
 }
+
+render() {
+  const { songs, collection, artist } = this.state;
+  return (
+    <div data-testid="page-album">
+      <Header />
+      <section>
+        <h3 data-testid="artist-name">{ artist }</h3>
+        <h4 data-testid="album-name">{ collection }</h4>
+        {songs.map((song, index) => (index > 0 && <MusicCard
+          key={ song.trackName }
+          trackName={ song.trackName }
+          previewUrl={ song.previewUrl }
+        />))}
+      </section>
+    </div>
+  );
+}
+}
+Album.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string,
+    }),
+  }),
+}.isRequired;
